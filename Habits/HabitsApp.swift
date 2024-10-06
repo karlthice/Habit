c56076a130aa19ec208b1439aa51e0f89fb1c1f0
Karl Thoroddsen <karl.thoroddsen@gmail.com>
2024-10-06 17:27:02 +0000

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Karl Thoroddsen on 6.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
